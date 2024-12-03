5e20c54f1dbd373dc4187374ed89aea5bb3a48f9
izabelly melo <izabelly.dmelo@mercadolivre.com>
2024-12-03 11:52:23 -0300

    adicionando código exemplo de como criar arquivo

---
+++ aula-manha/packageExerc/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	//Passo 1: Criar um arquivo
+	file, err := os.Create("dados.txt")
+	if err != nil {
+		fmt.Println("Erro ao criar o arquivo:", err)
+		return
+	}
+	defer file.Close() // Garante que o arquivo será fechad ao final
+
+	// Passo 2: Escrever dados no arquivo
+	_, err = file.WriteString("Olá, este é o meu primeiro arquivo em GO!\n")
+	if err != nil {
+		fmt.Println("Erro ao escrever no arquivo", err)
+		return
+	}
+	_, err = file.WriteString("Manipular arquivos é fácil em GO.\n")
+	if err != nil {
+		fmt.Println("Erro ao escrever no arquivo:", err)
+		return
+	}
+	fmt.Println("Dados escritos no arquivo com sucesso!")
+
+	// Passo 3: Abrir e ler o arquivo
+	data, err := os.ReadFile("dados.txt")
+	if err != nil {
+		fmt.Println("Erro ao ler o arquivo:", err)
+		return
+	}
+	// Passo 4: Exibir os dados no console
+	fmt.Println("\nConteúdo do arquvio:")
+	fmt.Println(string(data))
+}
